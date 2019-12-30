284da258330ed18cc5f6065cd207cbb7f3c29e77
Tyler Souza <tyler6souza@hotmail.com>
2019-12-30 15:19:00 -0500

    Updated ModUpdateLog
    Updated Modupdatelog Class to snippit Mister_Name Posted in group chat.

--- HjUpdaterAPI/HjUpdaterAPI.cs
+++ HjUpdaterAPI/HjUpdaterAPI.cs
@@ -597,8 +597,15 @@
         internal class ModUpdateLog
         {
             public string packageName { get; set; }
+            [JsonIgnore]
             public System.Version lastVersion { get; set; }
+            [JsonIgnore]
             public System.Version newVersion { get; set; }
+
+            [JsonPropertyAttribute("lastVersion")]
+            public string _lastVersionString => lastVersion.ToString();
+            [JsonPropertyAttribute("newVersion")]
+            public string _newVersionString => newVersion.ToString();
 
             public ModUpdateLog(string packageName, System.Version lastVersion, System.Version newVersion)
             {
