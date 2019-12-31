74b4209808d58fcbede7d76f6ff94351745817ba
Tyler Souza <tyler6souza@hotmail.com>
2019-12-31 17:03:55 -0500

    Split up HjUpdaterAPI
    Seperated all internal classes

---
+++ HjUpdaterAPI/ModUpdateLog.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace HjUpdaterAPI
+{
+    internal class ModUpdateLog
+    {
+        public string packageName { get; set; }
+
+        [JsonIgnore]
+        public System.Version lastVersion { get; set; }
+
+        [JsonIgnore]
+        public System.Version newVersion { get; set; }
+
+        [JsonPropertyAttribute("lastVersion")]
+        public string _lastVersionString => lastVersion.ToString();
+
+        [JsonPropertyAttribute("newVersion")]
+        public string _newVersionString => newVersion.ToString();
+
+        public ModUpdateLog(string packageName, System.Version lastVersion, System.Version newVersion)
+        {
+            this.packageName = packageName;
+            this.lastVersion = lastVersion;
+            this.newVersion = newVersion;
+        }
+    }
+}
