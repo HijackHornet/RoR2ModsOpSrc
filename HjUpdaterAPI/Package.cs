28168333a7661256cdb73e9e14fef735a4271f2f
Hijack <northriver@laposte.net>
2020-01-01 15:34:16 +0100

    Refactor

--- HjUpdaterAPI/Package.cs
+++ HjUpdaterAPI/Package.cs
@@ -38,4 +38,57 @@ namespace HjUpdaterAPI
 
         #endregion Methods
     }
+
+    public class Version
+    {
+        #region Properties
+
+        [J("date_created")] public DateTimeOffset DateCreated { get; set; }
+
+        [J("dependencies")] public string[] Dependencies { get; set; }
+
+        [J("description")] public string Description { get; set; }
+
+        [J("downloads")] public long Downloads { get; set; }
+
+        [J("download_url")] public Uri DownloadUrl { get; set; }
+
+        [J("full_name")] public string FullName { get; set; }
+
+        [J("icon")] public Uri Icon { get; set; }
+
+        [J("is_active")] public bool IsActive { get; set; }
+
+        [J("name")] public string Name { get; set; }
+
+        [J("uuid4")] public Guid Uuid4 { get; set; }
+
+        [J("version_number")] public System.Version VersionNumber { get; set; }
+
+        [J("website_url")] public string WebsiteUrl { get; set; }
+
+        #endregion Properties
+    }
+
+    internal class Converter
+    {
+        #region Fields
+
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None
+        };
+
+        #endregion Fields
+    }
+
+    public static class Serialize
+    {
+        #region Methods
+
+        public static string ToJson(Package[] self) => JsonConvert.SerializeObject(self, Converter.Settings);
+
+        #endregion Methods
+    }
 }
